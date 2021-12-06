d751d35b35417a57a510dc8967a902af7de08523
neoan <stefan.roehrl@gmail.com>
2021-12-06 15:21:37 -0500

    notification

---
+++ component/Ui/lib/ui.notifyMixin.vue
@@ -0,0 +1,24 @@
+<template>
+  <ui-notification v-model:show="showNotification"></ui-notification>
+</template>
+<script>
+import uiNotification from "/vue/ui/lib/ui.notification";
+const notificationTemplate = `{{template}}`;
+export default {
+  components:{uiNotification},
+  data:()=>({
+    showNotification:false,
+    title: 'something'
+  }),
+  methods:{
+    notify(options){
+      const n = document.createDocumentFragment();
+      n.innerHtml = notificationTemplate;
+      this.$el.append(n)
+      console.log(n.innerHtml)
+      this.showNotification = true;
+      console.log(this.showNotification)
+    }
+  },
+}
+</script>
---
+++ component/WriteInterface/WriteInterface.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <img src="{{base}}asset/img/blua-blue-edit.png" alt="how to write" class="w-100p">
+    <img src="{{base}}asset/img/blua-blue-save.png" alt="how to save" class="w-100p">
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    console.log('attached: writeInterface')
+  },
+  template: `{{template}}`,
+}
+</script>
